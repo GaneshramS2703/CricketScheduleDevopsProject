9f98d7b8f8e004891ff27c697f917e5d9849856a
Ganesh Ram <ganeshramsundari@gmail.com>
2024-12-04 23:17:21 +0000

    restored files

--- matches/urls.py
+++ matches/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-
-import cricketschedule_project.matches.match_list
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -9,7 +7,7 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
 
-    path('', cricketschedule_project.matches.match_list.match_list, name='match_list'),
+    path('', views.match_list, name='match_list'),
     path('add/', views.add_match, name='add_match'),
     path('edit/<int:id>/', views.edit_match, name='edit_match'),
     path('delete/<int:id>/', views.delete_match, name='delete_match'),
